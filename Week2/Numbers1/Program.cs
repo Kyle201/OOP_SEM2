73d5e55d2ab5c58d8deaf79dbb36b519da9fe7f7
KYLE GARVIE <102142522@student.swin.edu.au>
2018-08-06 14:50:35 +1000

    initial commit

---
+++ Week2/Numbers1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Numbers1
+{
+    class Program
+    {
+        static int FibonacciSeries(int n)
+        {
+            int a = 0, b = 1, result = 0;
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = a + b;
+                a = b;
+                b = result;
+            }
+
+            return result;
+
+        }
+        static void Main()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(FibonacciSeries(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
